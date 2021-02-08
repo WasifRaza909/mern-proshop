7572a27c06bd10caacf360e699f90c20a574c99a
wasifraza909 <wasifraza909@gmail.com>
2021-02-08 14:22:24 +0500

    Done with cartScreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -24,7 +24,7 @@ router.get(
     const product = await Product.findById(req.params.id);
 
     if (product) {
-      res.json(product);
+      return res.json(product);
     } else {
       res.status(404);
       throw new Error('Product not found');
