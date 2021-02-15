1323b2e9c520b92848ac3a0a20b9b52b42f89e78
wasifraza909 <wasifraza909@gmail.com>
2021-02-15 15:58:46 +0500

    Created Get user by id & Update user Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,6 +130,57 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    // user.isAdmin =
+    //   req.body.isAdmin === null
+    //     ? user.isAdmin
+    //     : req.body.isAdmin === true ||
+    //       (req.body.isAdmin === false && req.body.isAdmin);
+
+    if (req.body.isAdmin === true) {
+      user.isAdmin = true;
+    } else if (req.body.isAdmin === false) {
+      user.isAdmin = false;
+    } else {
+      user.isAdmin = user.isAdmin;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -137,4 +188,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
