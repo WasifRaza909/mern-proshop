f40b8d78855a3240f536956a136e7f7c0e1ad473
wasifraza909 <wasifraza909@gmail.com>
2021-02-15 14:34:47 +0500

    Created Admin User List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           {/* This questioon mark is for optional */}
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userList' component={UserListScreen} />
           <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
