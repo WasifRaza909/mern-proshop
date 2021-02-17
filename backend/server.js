5488c32283cafbb265db08f9231ab07e15c0e1d7
wasifraza909 <wasifraza909@gmail.com>
2021-02-17 17:32:56 +0500

    Added Image upload functionality and backend route, Created Admin Order List & also added functionality to mark order as delivered with backend route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+//   bcrypt used for encrypt password, colors is used to color console, dotenv is used for
+// env var jsonwebtoken is used to generate token multer is used for sending different format files
+// concurrently is used to start backend and frontend together and nodemon is used to not restart server again and again
+
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -7,6 +12,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -23,13 +29,18 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-app.use(notFound);
+// We have to make static folder in order to use uploads folder
+const __dirname = path.resolve();
+// __dirname is used in old js and its not available in es modules so we have to make a variable like above to mimic it.
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+// axios is used to send requests to backend, react-paypal-button-v2 is used to link with paypal,
+
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
@@ -13,6 +15,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import OrderListScreen from './screens/OrderListScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
@@ -37,8 +40,8 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/orderList' component={OrderListScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-
           <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
