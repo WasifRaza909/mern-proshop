e6bbfbb641ba1b47004b69865dbd8980a79f1299
wasifraza909 <wasifraza909@gmail.com>
2021-02-19 19:23:08 +0500

    Created FrontEnd Product Reviews section, Added Product Search & Product Pagination components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,10 +39,26 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route
+            path='/admin/productlist'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
           <Route path='/admin/orderList' component={OrderListScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} />
           <Route path='/' exact component={HomeScreen} />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+            exact
+          />
         </Container>
       </main>
       <Footer />
