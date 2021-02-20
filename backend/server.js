e5d20eb6d5ebbc4137cccfc2a1b273df9aa5ebe2
wasifraza909 <wasifraza909@gmail.com>
2021-02-20 15:50:56 +0500

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-//   bcrypt used for encrypt password, colors is used to color console, dotenv is used for
+// bcrypt used for encrypt password, colors is used to color console, dotenv is used for
 // env var jsonwebtoken is used to generate token multer is used for sending different format files
 // concurrently is used to start backend and frontend together and nodemon is used to not restart server again and again
 // Morgan consoles route,http req, seconds and response to console when hitting a route
@@ -28,10 +28,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -45,6 +41,18 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 // __dirname is used in old js and its not available in es modules so we have to make a variable like above to mimic it.
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
